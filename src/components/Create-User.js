a90647b8695fdcf41cc7d8bea78e94ae463a292e
laurenlittle <littlelauren016@gmail.com>
2019-08-13 21:15:57 -0400

    add post routes to create exercise and user

--- src/components/Create-User.js
+++ src/components/Create-User.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
 
@@ -27,6 +28,8 @@ export default class CreateUser extends Component {
     };
 
     console.log(newUser);
+    axios.post('http://localhost:5000/users/add', newUser)
+      .then(res => console.log(res.data));
 
     this.setState({
       username: ''
